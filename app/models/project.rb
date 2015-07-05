156adca20c2d73d5fcb3ea09371596e8da46b697
catty3003 <catty_chan16@yahoo.com>
2015-07-05 02:36:14 +0200

    is_delayed? Methode in Task eingefügt

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,9 @@ class Project < ActiveRecord::Base
 
 	validates :name, :deadline, presence: true
 	validates :duration, presence: true, numericality: true 
+
+	def is_delayed?
+	 self.deadline < Date.today
+	end
+	
 end
--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -3,16 +3,32 @@ require 'test_helper'
 class ProjectTest < ActiveSupport::TestCase
 
 	test "project can not be saved without name" do
- 	project = Project.new
- 	assert !project.save
-end
+	 	project = Project.new
+	 	assert !project.save
+	end
 
-test "project can be saved with name, deadline and duraton" do
- project = Project.new
- project.name = "Eine Frage"
- project.deadline = Date.today + 7.days
- project.duration = 5.0
- assert project.save
-end 
+	test "project can be saved with name, deadline and duraton" do
+	 project = Project.new
+	 project.name = "Eine Frage"
+	 project.deadline = Date.today + 7.days
+	 project.duration = 5.0
+	 assert project.save
+	end 
+
+	test "is delayed" do
+	 project = Project.new
+	 project.deadline = Date.today - 10.days
+	 assert project.is_delayed?
+	end
+	test "is not delayed" do
+	 project = Project.new
+	 project.deadline = Date.today + 10.days
+	 assert !project.is_delayed?
+	end
+	test "is not delayed if deadline is today" do
+	 project = Project.new
+	 project.deadline = Date.today
+	 assert !project.is_delayed?
+	end
 
 end
