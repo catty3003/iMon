d8051392ba443585c1b9de63b7d19c2821437c03
catty3003 <catty_chan16@yahoo.com>
2015-07-27 08:24:33 +0200

    bildupload ok

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,7 +4,18 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
+
+  version :thumb do
+    process :resize_to_fit => [200, 200]
+  end
+
+  version :thumb2 do
+    process :resize_to_fit => [100, 100]
+  end
+  version :normal do
+    process :resize_to_fit => [400, 400]
+  end
 
   # Choose what kind of storage to use for this uploader:
   storage :file
