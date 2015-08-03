23befb7d28a1b19d99ae0c1a07bb3447d41a02aa
catty3003 <catty_chan16@yahoo.com>
2015-08-03 04:47:25 +0200

    set_user_identification war falsch

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -39,8 +39,7 @@ class QuestionsController < ApplicationController
   # POST /questions
   # POST /questions.json
   def create
-    @question = Question.new(question_params)
-
+    @question = current_user.questions.new(question_params)
     respond_to do |format|
       if @question.save
         format.html { redirect_to edit_question_path(@question), notice: 'Questionnaire was successfully created.' }
